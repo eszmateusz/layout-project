5769cd7ef207f2423c170bf62cfceb93417f9dcc
Mateusz Szymonik <mateusz.szymonik@gmail.com>
2020-12-08 14:55:13 +0100

    c.d. fix mobile - not good meeh.

--- js/script.js
+++ js/script.js
@@ -4,27 +4,21 @@ function clickHamburgerButton() {
   const topBar = document.querySelector('.top-bar');
 
   const hambMenu = document.querySelector('.container .container-sidebar .burger-menu .button');
-  //const textMenu = document.querySelector('.burger-menu .text');
   const underMenu = document.querySelector('.under-menu');
 
   const containerSidebar = document.querySelector('.container-sidebar');
   const sidebar = document.querySelector('.sidebar');
   const container = document.querySelector('.container');
-  console.log(hambMenu);
-  console.log(topBar);
 
   button.addEventListener('click', function() {
     const thisButton = this;
     const button = thisButton.querySelector('.box-line');
     const sidebarMenu = document.querySelector('.list-menu');
-    console.log(sidebarMenu);
-    console.log(button);
 
     button.classList.toggle('active');
     sidebarMenu.classList.toggle('hidden');
     topBar.classList.toggle('close-menu');
     underMenu.classList.toggle('close-menu');
-    //textMenu.classList.toggle('close-menu');
     sidebar.classList.toggle('close-menu');
     containerSidebar.classList.toggle('close-menu');
     hambMenu.classList.toggle('vis-hamb');
