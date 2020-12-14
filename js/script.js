42c4c413981f17c4bb742f9cc1ec2d704891484b
Mateusz Szymonik <mateusz.szymonik@gmail.com>
2020-12-14 21:15:45 +0100

    Fix bugs for mobile versions.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
 function clickHamburgerButton(){
   const button = document.querySelector('.hamb-menu');
   const topBar = document.querySelector('.top-bar');
+
   const hambMenu = document.querySelector('.container .container-sidebar .burger-menu .button');
   const textMenu = document.querySelector('.burger-menu .text');
   const underMenu = document.querySelector('.under-menu');
+
   const containerSidebar = document.querySelector('.container-sidebar');
   const sidebar = document.querySelector('.sidebar');
   const container = document.querySelector('.container');
-  console.log(hambMenu);
-  console.log(topBar);
+  //console.log(hambMenu);
+  //console.log(topBar);
+
   button.addEventListener('click', function(){
     const thisButton = this;
+
     const button = thisButton.querySelector('.box-line');
     const sidebarMenu = document.querySelector('.list-menu');
-    console.log(sidebarMenu);
-    console.log(button);
+    //console.log(sidebarMenu);
+    //console.log(button);
+
     button.classList.toggle('active');
     sidebarMenu.classList.toggle('hidden');
     topBar.classList.toggle('close-menu');
